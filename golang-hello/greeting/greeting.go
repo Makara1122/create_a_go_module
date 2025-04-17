e2c2cc0f209f4f9be3c3f66dbf01e0451ffd1974
Makara1122 <mam.makara.1221@rupp.edu.kh>
2025-04-17 14:29:57 +0700

    handle error exception

--- golang-hello/greeting/greeting.go
+++ golang-hello/greeting/greeting.go
@@ -1,10 +1,17 @@
 package greeting
 
-import "fmt"
+import (
+	"fmt"
+	"errors"
+)
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// Check if the name is empty and return an error if it is.
+	if name == "" {
+		return "", errors.New("name cannot be empty")
+	}
     // Return a greeting that embeds the name in a message.
     message := fmt.Sprintf("Hi, %v. Welcome!", name)
-    return message
+    return message,	nil
 }
--- golang-hello/hello/hello.go
+++ golang-hello/hello/hello.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"fmt"
-	
+	"log"
 	"greeting"
 )
 
 func main() {
+	// Set properties of the predefined logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number
+	log.SetPrefix("greeting: ")
+	log.SetFlags(0)
+
 	// Get a greeting message and print it.
-	message := greeting.Hello("World")
+	message,err := greeting.Hello("")
+	// If an error was returned, print it to the console and 
+	// exit the program
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console
 	fmt.Println(message)
 }
